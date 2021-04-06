f30cce96005810089bfab2c6ddd698f9a0a93acd
samsambutdifferent <sam@solutions.to>
2021-04-06 22:22:06 +0100

    firestore helper logger

--- firestore_helper.py
+++ firestore_helper.py
@@ -4,10 +4,13 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+from datetime import datetime
+
+
 cred = credentials.Certificate('./key.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
-firebase_collection = 'carbon_matches'
+firebase_collection = 'categories'
 
 def load_carbon_matches():
     """loads matches from firestore db
@@ -27,3 +30,27 @@ def load_carbon_matches():
     return carbon_matches
 
 
+def write_to_reported(log_id, matched, original):
+    """writes value to reported firebase table
+        params:
+            log_id
+                type: string
+            matched
+                type: string
+            original
+                type: string
+    """
+    date_time = datetime.now().strftime("%m/%d/%Y-%H:%M:%S")
+    data = {
+        u'log_id': log_id,
+        u'matched': matched,
+        u'original': original,
+        u'reviewed': False
+    }
+
+    print(f"reporting: {data}")
+
+    try:
+        db.collection(u'reported').document(date_time).set(data)
+    except Exception as e:
+        print(f"failed reporting: {e}")
